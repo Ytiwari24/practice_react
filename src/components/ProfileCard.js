86d540f337987405212b25632f6fe1b98d878efd
Pravind <parvindraj004@gmail.com>
2023-12-28 14:35:43 +0530

    added image

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,15 +1,20 @@
+// ProfileCard.js
 import React from 'react';
 import '../css/profile_card.css';
 import Profiles from '../data/constants';
 
 function ProfileCard() {
   return (
-    
     <div className="profile-container">
-        
       {Profiles.map((profile, index) => (
         <div className="profile-card" key={index}>
-          <div className="profile-image"></div>
+          <div className="profile-image">
+            <img
+              src={profile.imageSrc} // Replace with the actual image source from your data
+              alt={profile.name}
+              className="profile-img"
+            />
+          </div>
           <div className="profile-details">
             <h2>{profile.name}</h2>
             <p>{profile.expertise}</p>
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,12 +1,44 @@
+import ProfileImage from '../components/images/profile.jpg';
+import NewImage from '../components/images/new.jpg';
+
 const Profiles = [
-    { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
-    { name: 'John Doe', expertise: 'React Developer' },
-    { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
-    { name: 'John Doe', expertise: 'React Developer' }, 
-    { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
-    { name: 'John Doe', expertise: 'React Developer' }, 
-    { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
-    { name: 'John Doe', expertise: 'React Developer' },
+    {
+        name: 'Rocket',
+        expertise: 'Space Guard',
+        imageSrc: `${NewImage}`,
+    },
+    {
+        name: 'John Doe',
+        expertise: 'Web Developer',
+        imageSrc: `${ProfileImage}`,
+    },
+    {
+        name: 'Rocket',
+        expertise: 'Space Guard',
+        imageSrc: `${NewImage}`,
+    },
+    {
+        name: 'John Doe',
+        expertise: 'Web Developer',
+        imageSrc: `${ProfileImage}`,
+    },    {
+        name: 'Rocket',
+        expertise: 'Space Guard',
+        imageSrc: `${NewImage}`,
+    },
+    {
+        name: 'John Doe',
+        expertise: 'Web Developer',
+        imageSrc: `${ProfileImage}`,
+    },
+    // { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
+    // { name: 'John Doe', expertise: 'React Developer' },
+    // { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
+    // { name: 'John Doe', expertise: 'React Developer' }, 
+    // { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
+    // { name: 'John Doe', expertise: 'React Developer' }, 
+    // { name: 'Pravind Bhardwaj', expertise: 'Flutter Developer' },
+    // { name: 'John Doe', expertise: 'React Developer' },
 
 ];
 
